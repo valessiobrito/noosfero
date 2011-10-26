08ce13ca46ba6d3628bf6f46e9feaedc81859cc2
Aurelio A. Heckert <aurelio@colivre.coop.br>
2011-10-26 17:50:06 -0200

    Recognize inputs with html5 placeholder
    Closes ActionItem2109

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -695,3 +695,30 @@ jQuery(function() {
     target: "#ajax-form-message-area"
   })
 });
+
+// Input Placeholder Fix
+// http://www.hagenburger.net/BLOG/HTML5-Input-Placeholder-Fix-With-jQuery.html
+jQuery(function($) {
+  $("[placeholder]").focus(function() {
+    var input = $(this);
+    if (input.val() == input.attr("placeholder")) {
+      input.val("");
+      input.removeClass("placeholder");
+    }
+  }).blur(function() {
+    var input = $(this);
+    if (input.val() == "" || input.val() == input.attr("placeholder")) {
+      input.addClass("placeholder");
+      input.val(input.attr("placeholder"));
+    }
+  }).blur();
+  $("[placeholder]").parents("form").submit(function() {
+    $(this).find("[placeholder]").each(function() {
+      var input = $(this);
+      if (input.val() == input.attr("placeholder")) {
+        input.val("");
+      }
+    })
+  });
+});
+
